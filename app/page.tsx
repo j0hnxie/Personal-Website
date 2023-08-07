76811bbbaf2215de08e7cd5025485c3a50a8e6d6
j0hnxie <trashads7@gmail.com>
2023-08-07 12:55:08 -0400

    Fixed apostrophe bug

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
                 Computer Science and Business at the University of Michigan — Ann Arbor
             </div>
             <div className="text-body mt-8 font-extralight">
-                I am a passionate learner who hopes to leverage technologies in order to achieve impact on large scales. I've had the privilege of building a variety software ranging from data analysis for a F500 company to app development for a fast-growing startup.
+                I am a passionate learner who hopes to leverage technologies in order to achieve impact on large scales. I&apos;ve had the privilege of building a variety software ranging from data analysis for a F500 company to app development for a fast-growing startup.
             </div>
             <div className="text-body mt-8 font-extralight">
-                My main focus these days is exploring new sides of Computer Science ranging from general software, system design, data analysis, app development, and much much more. Most recently, I've worked on <Link href="https://github.com/j0hnxie/Personal-Website" rel="noopener noreferrer" target="_blank" className='font-bold'>this personal website</Link> to explore the depths of web development. Come check out the <Link href="/projects" className='font-bold'>rest of my projects here</Link>
+                My main focus these days is exploring new sides of Computer Science ranging from general software, system design, data analysis, app development, and much much more. Most recently, I&apos;ve worked on <Link href="https://github.com/j0hnxie/Personal-Website" rel="noopener noreferrer" target="_blank" className='font-bold'>this personal website</Link> to explore the depths of web development. Come check out the <Link href="/projects" className='font-bold'>rest of my projects here</Link>
            </div>
             <div className="text-body mt-8 font-extralight">
-                When I’m not at the computer, I’m usually playing volleyball, trying out all kinds of foods, or enjoying new movies.
+                When I&apos;m not at the computer, I&apos;m usually playing volleyball, trying out all kinds of foods, or enjoying new movies.
             </div>
         </div>
         <div className="flex flex-col flex-1 pt-24">
