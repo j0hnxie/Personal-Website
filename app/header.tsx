36116af044fcb82c2e47e59f4eb7167fb809f615
j0hnxie <trashads7@gmail.com>
2024-03-01 02:12:48 -0500

    Updated resume

--- app/header.tsx
+++ app/header.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-import { AiFillGithub, AiOutlineMail, AiFillLinkedin, AiOutlineMenu } from 'react-icons/ai'
-import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'; 
-import MobileMenu from './mobile-menu';
+import {
+  AiFillGithub,
+  AiOutlineMail,
+  AiFillLinkedin,
+  AiOutlineMenu,
+} from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import MobileMenu from "./mobile-menu";
 
 export default function Header() {
   const [mobile, setMobile] = useState(false);
@@ -14,77 +19,95 @@ export default function Header() {
 
   const handleMenuOpen = () => {
     setMenuOpen(true);
-  }
+  };
 
   // window.onresize = () => {
   //   setWindowChange(!windowChange)
   //   console.log(window.screen.width)
   // }
-  
+
   useEffect(() => {
     if (window.screen.width < 640) {
-      setMobile(true)
+      setMobile(true);
     } else {
-      setMobile(false)
+      setMobile(false);
     }
-}, [windowChange])
+  }, [windowChange]);
 
   return (
     <div className="flex flex-col bg-navy-blue bg-opacity-100 w-full fixed z-30">
-    {
-        menuOpen ? (
-          <div className="flex h-0 justify-center">
-            <MobileMenu setMenuOpen={setMenuOpen}/>
-          </div>
-        ) : (<div/>)
-      }
+      {menuOpen ? (
+        <div className="flex h-0 justify-center">
+          <MobileMenu setMenuOpen={setMenuOpen} />
+        </div>
+      ) : (
+        <div />
+      )}
       <div className="flex flex-row p-8 justify-between items-center">
         <div>
-            <Link href="/">
-              <div className='text-body font-bold' >
-                J.X
-              </div>
-            </Link>
+          <Link href="/">
+            <div className="text-body font-bold">J.X</div>
+          </Link>
         </div>
-        {
-          mobile ? (
-            <div onClick={handleMenuOpen}>
-              <AiOutlineMenu size={30}/>
-            </div>
-          ) : (
-            
+        {mobile ? (
+          <div onClick={handleMenuOpen}>
+            <AiOutlineMenu size={30} />
+          </div>
+        ) : (
           <div className="flex flex-row space-x-8 items-center">
             <div className="flex flex-row space-x-8">
-              <Link href="/" className={pathname === "/" ? "border font-medium p-2 rounded-3xl" : "self-center hover:text-hover-blue"}>
+              <Link
+                href="/"
+                className={
+                  pathname === "/"
+                    ? "border font-medium p-2 rounded-3xl"
+                    : "self-center hover:text-hover-blue"
+                }
+              >
                 Home
               </Link>
-              <Link href="/projects" className={pathname.startsWith("/projects") ? "border font-medium p-2 rounded-3xl" : "self-center hover:text-hover-blue"}>
+              <Link
+                href="/projects"
+                className={
+                  pathname.startsWith("/projects")
+                    ? "border font-medium p-2 rounded-3xl"
+                    : "self-center hover:text-hover-blue"
+                }
+              >
                 Projects
               </Link>
-              <Link href="/personal" className={pathname.startsWith("/personal") ? "border font-medium p-2 rounded-3xl" : "self-center hover:text-hover-blue"}>
+              <Link
+                href="/personal"
+                className={
+                  pathname.startsWith("/personal")
+                    ? "border font-medium p-2 rounded-3xl"
+                    : "self-center hover:text-hover-blue"
+                }
+              >
                 Personal
               </Link>
-              <Link href="https://drive.google.com/file/d/16EdD8f9sqRxFmxeDiaim3OO4okiy2b0Q/view?usp=sharing" className="self-center hover:text-hover-blue">
+              <Link
+                href="https://drive.google.com/file/d/1SCUOV91QxZZTDifCg1HBjqQjDDsc2WVH/view?usp=sharing"
+                className="self-center hover:text-hover-blue"
+              >
                 Resume
               </Link>
             </div>
             <div className="flex flex-row space-x-4">
               <Link href="https://github.com/j0hnxie">
-                <AiFillGithub size={30}/>
+                <AiFillGithub size={30} />
               </Link>
               <Link href="mailto:johnxie@umich.edu">
-                <AiOutlineMail size={30}/>
+                <AiOutlineMail size={30} />
               </Link>
               <Link href="https://www.linkedin.com/in/john-xie-7437a9203/">
-                <AiFillLinkedin size={30}/>
+                <AiFillLinkedin size={30} />
               </Link>
             </div>
           </div>
-          )
-        }
+        )}
       </div>
-      <hr>
-      </hr>
+      <hr></hr>
     </div>
-  )
+  );
 }
